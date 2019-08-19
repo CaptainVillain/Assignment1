dfd70332cc8ab3bd449bbef3c2f79ba67439f0b9
CaptainVillain <CaptainVillain@users.noreply.github.com>
2019-08-19 22:16:53 +0200

    push

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() => print("Hello world");
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            print("Respect");
+        }
+    }
+
+    public GameObject SpawnCube;
+
+    public void MakeCube()
+    {
+        Instantiate(SpawnCube, new Vector3(5, 5, 5), Quaternion.identity);
+    }
+}
